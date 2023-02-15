6b57ea9add52358a463fce97465c5b9db75aaf78
Brandon Lyman <66562222+blyman94@users.noreply.github.com>
2023-02-14 22:59:53 -0500

    First final update

--- LevelDesignProject/Assets/Scripts/InteractableObject.cs
+++ LevelDesignProject/Assets/Scripts/InteractableObject.cs
@@ -7,10 +7,36 @@ public class InteractableObject : MonoBehaviour
 {
     [SerializeField] private string _stringToDisplay;
     [SerializeField] private UnityEvent _OnInteractResponse;
+    public bool IsTimeActivated;
+    [SerializeField] private float _timeToActivate;
+
+    public bool IsBeingLookedAt { get; set; }
+
+    private float _elapsedTime;
+
+    private void Start()
+    {
+        _elapsedTime = 0.0f;
+    }
+
+    private void Update()
+    {
+        if (IsBeingLookedAt)
+        {
+            _elapsedTime += Time.deltaTime;
+            if (_elapsedTime >= _timeToActivate)
+            {
+                _OnInteractResponse?.Invoke();
+            }
+        }
+    }
 
     public void Activate()
     {
-        _OnInteractResponse?.Invoke();
+        if (IsTimeActivated)
+        {
+            _OnInteractResponse?.Invoke();
+        }
     }
 
     public string GetInteractionString()
